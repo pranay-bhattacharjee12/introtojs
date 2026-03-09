2eb736f4c08fa3f5b2e1f7293ebf5f15504a0914
pranay-bhattacharjee12 <pranaybhattacharjee66@gmail.com>
2026-03-09 23:38:50 +0530

    commite somemodification

--- 06String/06String.js
+++ 06String/06String.js
@@ -10,16 +10,16 @@ console.log(name.length)
 //to convert the string ontouppercase
 console.log(greet.toUpperCase())
 
-//to check the which letter i spresent in that position
+//to check the which letter is present in that position
 console.log(name.charAt(0))
 
 //to check the indexof the letter in the string
 console.log(greet.indexOf("e"))
 
-//slice 
-// console.log(name.slice(-8,3))
+// //slice 
+console.log(name.slice(-8,3))  //Rih
 
-//remove the extra spaces
+// //remove the extra spaces
 let a ="   aryaan   "
 console.log(a.trim())
 
--- 07arrays/07arrays.js
+++ 07arrays/07arrays.js
@@ -2,11 +2,11 @@ const arr= [12,423,23,32,67]
 console.log(arr[1]);
 
 //Arrays methhod
-//push in given arr
+//push -> add element to the end
 arr.push(1);
 console.log(arr)
 
-//popout last element
+//pop -> remove element from the end
 console.log(arr.pop());
 
 const orga = [12,13,14,34,56,89]
@@ -16,15 +16,15 @@ const ar1 = orga.slice(1,3);
 console.log(orga);
 console.log(ar1);
 
-//splice -> manipulate the original array 
+//splice -> manipulate(modifies) the original array 
 const ar2 = orga.splice(1,3)
 console.log(orga);      //only print which are notincluded
 console.log(ar2);
 
-//spredopertaor -> used to spred the array 
+//spredopertaor -> used to combine arrays 
 console.log("using spredoperator : " + [...arr, ...orga]);
 
-//flat -> used to join the all arrays and return new array 
+//flat -> used to join the all arrays and return new array (flatten nested arrays)
 const arr3 = [1, 2, [23,43,56], 78,55,[4,6,7,9], 9,97 ]
 console.log(`after using flat ${arr3.flat(Infinity)}`)        // .flat(depth)
 
